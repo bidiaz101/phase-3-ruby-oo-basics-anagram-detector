5332a66d1f31b7d574d0bf56f20db452c1fc3dfc
Brian Diaz <bidiaz101@gmail.com>
2021-10-18 05:15:47 -0500

    commit tree 0df99c971ba5de80dccc8767d3140bd667041852

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_arr)
+        word_arr.filter {|word| word.chars.sort == @word.chars.sort}
+    end
+end
